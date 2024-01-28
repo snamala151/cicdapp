6b15a785dcba1e15d8a8864b3c5086cd9dca1b54
Surya Namala <surya.namala@gmail.com>
2024-01-27 22:07:45 -0500

    Adding logger

--- src/test/java/com/suryatech/cicdapp/CicdappApplicationTests.java
+++ src/test/java/com/suryatech/cicdapp/CicdappApplicationTests.java
@@ -3,11 +3,14 @@ package com.suryatech.cicdapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
 @SpringBootTest
 class CicdappApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals("Testing","Test","Test");
 	}
 
 }
