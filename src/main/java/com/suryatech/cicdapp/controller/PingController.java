7c45a3c18a58d28e74196fcae0e724e97e1bec15
Surya Namala <surya.namala@gmail.com>
2024-01-27 22:05:07 -0500

    Adding logger

--- src/main/java/com/suryatech/cicdapp/controller/PingController.java
+++ src/main/java/com/suryatech/cicdapp/controller/PingController.java
@@ -1,13 +1,18 @@
 package com.suryatech.cicdapp.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingController
 {
+    Logger logger
+            = LoggerFactory.getLogger(PingController.class);
     @GetMapping("/ping")
     public String hello(){
+        logger.info("Ping controller is going to return the sample log message");
         return "Ping pong";
     }
 }
